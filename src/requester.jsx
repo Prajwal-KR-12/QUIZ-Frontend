e481b912a71e5f7928b72b1728066f834843c13d
Prajwal KR <prajwalkr1227@gmail.com>
2025-10-22 22:51:16 +0530

    Initial commit of frontend

---
+++ src/requester.jsx
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const requester = axios.create({
+  baseURL: 'https://quiz-backend-2-3tsk.onrender.com//api', // Assuming your backend runs on port 5000 and has an /api prefix
+  withCredentials: true, // Important for sending cookies with requests
+});
+
+export const registerUser = async (userData) => {
+  try {
+    const response = await requester.post('/auth/register', userData);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export default requester;
